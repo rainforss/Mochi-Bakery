1004f201e4d64970468d0b3b97d9cabb024423ab
rainforss <shengyan@ualberta.ca>
2020-10-13 16:47:18 -0600

    Added useCart custom hook

--- components/common/ShoppingCart.js
+++ components/common/ShoppingCart.js
@@ -2,19 +2,21 @@ import React from "react";
 import Link from "next/link";
 import { BagFill } from "react-bootstrap-icons";
 import { Badge } from "react-bootstrap";
+import Cookies from "js-cookie";
 import useSWR from "swr";
-import axios from "axios";
+import useCart from "../../hooks/useCart";
 
 const ShoppingCart = () => {
-  const { data, error } = useSWR("https://api.chec.io/v1/carts");
-  if (error) return error.message;
-  if (!data) return "Loading";
-  console.log(data);
+  const currentCartId = Cookies.get("commercejs_cart_id");
+  console.log(currentCartId);
+  let cart = useCart(currentCartId);
+  console.log(cart);
+
   return (
     <Link href="/cart">
       <div className="fixed-cart">
         <BagFill />
-        <Badge></Badge>
+        <Badge variant="danger">{cart.total_items}</Badge>
       </div>
     </Link>
   );
