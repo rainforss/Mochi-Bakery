8ed2a754120e8cccd7e8025d24b735b805d40ef8
rainforss <shengyan@ualberta.ca>
2020-10-25 13:17:15 -0600

    Added meta tags for basic SEO

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,36 @@
 import React from "react";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
-    <div className="body">
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Best cake shop in Edmonton specialized in trending cakes and sweets"
+        />
+        <meta name="title" property="og:title" content="Mochi's Bakery" />
+        <meta property="og:type" content="Website" />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://res.cloudinary.com/rainforss/image/upload/v1603652748/website/mochibakery_gnkzft.png"
+        />
+        <meta
+          name="description"
+          property="og:description"
+          content="Best cake shop in Edmonton specialized in trending cakes and sweets"
+        />
+        <meta name="author" content="Jake Chen" />
+      </Head>
+      <div className="body">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 };
 
--- pages/products/index.js
+++ pages/products/index.js
@@ -45,23 +45,6 @@ const products = ({ allProducts, categories }) => {
       <Layout>
         <Head>
           <title>Mochi's Bakery | Products</title>
-          <meta
-            name="description"
-            content="Best cake shop in Edmonton specialized in trending cakes and sweets"
-          />
-          <meta name="title" property="og:title" content="Mochi's Bakery" />
-          <meta property="og:type" content="Website" />
-          <meta
-            name="image"
-            property="og:image"
-            content="https://res.cloudinary.com/rainforss/image/upload/v1603652748/website/mochibakery_gnkzft.png"
-          />
-          <meta
-            name="description"
-            property="og:description"
-            content="Best cake shop in Edmonton specialized in trending cakes and sweets"
-          />
-          <meta name="author" content="Jake Chen" />
         </Head>
         <WeeklySpecialSection
           weeklySpecials={weeklySpecials}
